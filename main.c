0cea0546050aa007eca15545686eaf92250f3522
D <bzsombi99@gmail.com>
2021-10-29 09:33:28 +0200

    parancsbeolvasás

--- main.c
+++ main.c
@@ -5,30 +5,37 @@
 typedef enum {segitseg, beolvas, kiir, mentes, megallo, utvonal, kilep, hibas} parancsok;
 
 int str_to_parancs(char parancs[]) {
+    // leszedjük az első szóközig/sorvégéig tartó stringet, ez lesz maga a parancs
     char temp_parancs[50+1];
     int i = 0;
-    for (i = 0; parancs[i] != ' '; ++i) {
+    while (parancs[i] != ' ' && parancs[i] != '\n') {
         temp_parancs[i] = parancs[i];
+        i++;
     }
     temp_parancs[i] = '\0';
 
-    for (int j = i; temp_parancs[i]; ++i) {
-
+    // a parancsból leszedjük az előbb kiválasztott parancsot
+    // átirjuk a parancsot kihagyva az összes elemet, az elejétől kezdődően
+    int k = 0;
+    int j = i;
+    while (parancs[j] != '\n') {
+        parancs[k++] = parancs[j++];
     }
+    parancs[j] = '\0';
 
-    if (strcmp(temp_parancs, "segitseg"))
+    if (strcmp(temp_parancs, "segitseg") == 0)
         return segitseg;
-    else if (strcmp(temp_parancs, "beolvas"))
+    else if (strcmp(temp_parancs, "beolvas") == 0)
         return beolvas;
-    else if (strcmp(temp_parancs, "kiir"))
+    else if (strcmp(temp_parancs, "kiir") == 0)
         return kiir;
-    else if (strcmp(temp_parancs, "mentes"))
+    else if (strcmp(temp_parancs, "mentes") == 0)
         return mentes;
-    else if (strcmp(temp_parancs, "megallo"))
+    else if (strcmp(temp_parancs, "megallo") == 0)
         return megallo;
-    else if (strcmp(temp_parancs, "utvonal"))
+    else if (strcmp(temp_parancs, "utvonal") == 0)
         return utvonal;
-    else if (strcmp(temp_parancs, "kilep"))
+    else if (strcmp(temp_parancs, "kilep") == 0)
         return kilep;
     else
         return hibas;
@@ -40,9 +47,8 @@ int main() {
     bool futas = true;
     char parancs[50+1];
     while (futas) {
-        printf("Parancs:");
-        fgets(parancs, 50, stdin);
-        printf("%s", parancs);
+        printf("Parancs(max 50 karakter):");
+        fgets(parancs, 51, stdin);
         switch (str_to_parancs(parancs)) {
             case segitseg:
                 printf("Parancsok: segitseg, beolvas, kiir, mentes, megallo, utvonal, kilep\n");
