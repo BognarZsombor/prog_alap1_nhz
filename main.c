fda3fb288131afae817020f8ac81c43b40f68446
D <52254412+BognarZsombor@users.noreply.github.com>
2021-10-30 21:58:58 +0200

    parancsok beolvása kész

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
 
@@ -15,33 +16,12 @@ typedef struct {
 
 typedef enum {segitseg, beolvas, kiir, mentes, megallo, utvonal, kilep, hibas} parancsok;
 
-/* str_to_parancs()
- * Visszaadja melyik előre definiált paranccsal egyenlő a beérkezett string.
- * A string eleji szóközök nélkül értelmezi a parancsot. A sor elejéről leszedi a parancsot és szóközöket.
- * @param char* sor A beérkező sor
- * @return parancsok közül ad vissza egyet
+/* str_to_parancs
+ * A beérkető string alapján visszaadja a hozzátartozó parancsot
+ * @param char parancs A beérkező string
+ * @return int A strinhez tartozó parancs enum alapján
  * */
-int str_to_parancs(char sor[]) {
-    // leszedjük az első szóközig/sorvégéig tartó stringet, ez lesz maga a parancs
-    char parancs[50+1];
-    int i = 0;
-    while (sor[i] == ' ')
-        i++;
-    int l = 0;
-    while (sor[i] != ' ' && sor[i] != '\n') {
-        parancs[l++] = sor[i++];
-    }
-    parancs[l] = '\0';
-
-    // a parancsból leszedjük az előbb kiválasztott parancsot
-    // átirjuk a parancsot kihagyva az összes elemet, az elejétől kezdődően
-    int k = 0;
-    int j = i+1;
-    while (sor[j] != '\n') {
-        sor[k++] = sor[j++];
-    }
-    sor[k] = '\0';
-
+int str_to_parancs(char *parancs) {
     if (strcmp(parancs, "segitseg") == 0)
         return segitseg;
     else if (strcmp(parancs, "beolvas") == 0)
@@ -60,16 +40,55 @@ int str_to_parancs(char sor[]) {
         return hibas;
 }
 
+/* sor_to_list
+ * Szétszedi a beadott sort egy stringekből álló listára, aminek az első eleme a parancs, a többi pedig a hozzá tartozó paraméterek.
+ * @param char* sor A beérkező string
+ * @return char** A szóközött alapján szétszedett stringlista
+ * */
+char **sor_to_list(char sor[]) {
+    //a sor eleji felesleges spacek kihagyása
+    int k;
+    for (k = 0; sor[k] == ' '; ++k) {
+        k++;
+    }
+
+    int spaces = 0;
+    for (int i = k; sor[i] != '\n'; ++i) {
+        if (sor[i] == ' ') {
+            spaces++;
+        }
+    }
+    char **temp_sor = (char**) malloc(spaces * sizeof(char*));
+
+    spaces = 0;
+    temp_sor[0] = malloc(51 * sizeof(char));
+    int j = 0;
+    for (int i = k; sor[i] != '\n'; ++i) {
+        if (sor[i] == ' ') {
+            temp_sor[spaces][j] = '\0';
+            spaces++;
+            j = 0;
+            temp_sor[spaces] = malloc(51 * sizeof(char));
+        } else {
+            temp_sor[spaces][j++] = sor[i];
+        }
+    }
+    temp_sor[spaces][j] = '\0';
+
+    return temp_sor;
+}
+
 int main() {
     bool futas = true;
-    char sor[50+1];
+    char sor[100+1]; //100 karakter, hogy beleféljen fájl elérési út is ha szükséges (magában 50 karakter kb)
     while (futas) {
-        printf("Parancs(max 50 karakter):");
-        if (fgets(sor, 51, stdin) == NULL) {
+        printf("Parancs(max 100 karakter):");
+        if (fgets(sor, 101, stdin) == NULL) {
             printf("Hiba a sor beolvasása közben.");
             return -1;
         }
-        switch (str_to_parancs(sor)) {
+        char **parancssor = sor_to_list(sor);
+        switch (str_to_parancs(parancssor[0])) {
             case segitseg:
                 printf("Parancsok: segitseg, beolvas, kiir, mentes, megallo, utvonal, kilep\n");
                 break;
